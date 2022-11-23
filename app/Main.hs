7408b04bc2aa4c8375fab72e27acd38d1821f224
Zachary Juang <zachary822@me.com>
2022-11-23 14:37:40 -0500

    Query post by oid

--- app/Main.hs
+++ app/Main.hs
@@ -4,13 +4,14 @@
 module Main where
 
 import Data.AesonBson
-import Data.Text qualified as T
 import Database.MongoDB hiding (value)
 import Lib.DbConfig
 import Lib.ServerOpts
 import Network.Wai.Middleware.RequestLogger (logStdout, logStdoutDev)
 import Options.Applicative
 import Web.Scotty
+import Text.Read (readMaybe)
+import Network.HTTP.Types.Status ( notFound404 )
 
 getPosts = do
   cur <- find (select [] "posts") {sort = ["updated" =: -1]}
@@ -27,9 +28,21 @@ main = do
   pipe <- primary replica
   _ <- access pipe master "admin" (auth (dbuser dbConfig) (dbpasswd dbConfig))
 
+  let run = access pipe master db
+
   scotty (port serverOpts) $ do
     middleware (if (debug serverOpts) then logStdoutDev else logStdout)
 
-    get "/" $ do
-      posts <- access pipe master db getPosts
+    get "/posts/" $ do
+      posts <- run getPosts
       json $ map aesonify posts
+
+    get "/posts/:oid" $ do
+      oidParam <- param "oid"
+
+      let oid = (readMaybe oidParam :: Maybe ObjectId)
+
+      maybePost <- run $ findOne (select ["_id" =: oid] "posts")
+
+      case maybePost of Just p -> json $ aesonify p
+                        Nothing -> status notFound404 >> text "Not Found"
